b2cf2c76e2d9f955257a9f61b8e6aa56513fd408
Don Branson <don.branson+w7@gmail.com>
2011-04-25 20:48:14 -0500

    import cleanup.

--- src/test/com/penwag/test/MyClass_UsualApproach_UT.java
+++ src/test/com/penwag/test/MyClass_UsualApproach_UT.java
@@ -4,7 +4,6 @@ import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.verify;
 
 import org.hamcrest.Description;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatcher;
